f087b25a4e539702bdc2108330fcc28e532bdc2f
rafa <terenrafa@hotmail.com>
2019-01-12 14:25:56 +0100

    documentos pdf de ejemplo insertados

---
+++ app/src/main/java/com/example/zafiro2/misrecetas/ManejoFicheros/VisorPDF.java
@@ -0,0 +1,15 @@
+package com.example.zafiro2.misrecetas.ManejoFicheros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zafiro2.misrecetas.R;
+
+public class VisorPDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visor_pdf);
+    }
+}
