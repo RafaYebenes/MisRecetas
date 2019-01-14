82d34fe63068fc52f246783758fa12219407215e
rafa <terenrafa@hotmail.com>
2019-01-14 01:36:40 +0100

    Commit 14/01/2019
    Creado el visor de PDF, lee todos los archivos menos el de dorada, dice que no encuentra el archivo. Todos los demas archivos se leen sin problema.

--- app/src/main/java/com/example/zafiro2/misrecetas/BBDDInterna/DataBaseOpenHelper.java
+++ app/src/main/java/com/example/zafiro2/misrecetas/BBDDInterna/DataBaseOpenHelper.java
@@ -7,7 +7,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DataBaseOpenHelper extends SQLiteAssetHelper {
 
-    private static final String DATABASE_NAME = "bbddRecetas";
+    private static final String DATABASE_NAME = "bbddRecetas3";
     private static final int DATABASE_VERSION = 1;
 
     public DataBaseOpenHelper(Context context){
--- app/src/main/java/com/example/zafiro2/misrecetas/ManejoFicheros/VisorPDF.java
+++ app/src/main/java/com/example/zafiro2/misrecetas/ManejoFicheros/VisorPDF.java
@@ -1,9 +1,12 @@
 package com.example.zafiro2.misrecetas.ManejoFicheros;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.zafiro2.misrecetas.R;
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class VisorPDF extends AppCompatActivity {
 
@@ -11,5 +14,11 @@ public class VisorPDF extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visor_pdf);
+        Intent intent = getIntent();
+        String enlace = intent.getStringExtra("enlace");
+
+        PDFView p = (PDFView)findViewById(R.id.pdfView);
+        p.fromAsset(enlace).load();
+
     }
 }
